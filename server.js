c264030ac2815ecaf124987d377203cb8f0d42a3
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-12-10 09:47:57 -0600

    Got Saved Notes to Appear.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const termData = require('./db/db.json');
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.use(express.static('./public'));
 
@@ -18,6 +21,9 @@ app.get('/index', (req, res) =>
 app.get('/notes', (req, res) =>
 	res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// Pulls notes from db.json file.
+app.get('/api/notes', (req, res) => res.json(termData));
 
 // Server setup to listen.
 app.listen(PORT, () => 
