b9e5a334df802f8daafeaeef70b53850c2ba7b1f
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-12-10 20:52:21 -0600

    Got POST Route to Succeed.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const termData = require('./db/db.json');
+const fs = require('fs');
+
 const PORT = 3000;
 
 const app = express();
 
+// Sets up the Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
@@ -12,18 +15,42 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
 
-// GET Route for homepage
+// GET Route for homepage.
 app.get('/index', (req, res) =>
 	res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
+// GET Route for notes page.
 app.get('/notes', (req, res) =>
 	res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Pulls notes from db.json file.
 app.get('/api/notes', (req, res) => res.json(termData));
+
+// POST Route for notes page.
+app.post('/api/notes', (req, res) => {
+	console.info(`${req.method} request received to add a review`);
+
+	const {title, text} = req.body;
+
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+		};
+
+		const response = {
+			status: 'success',
+			body: newNote,
+		};
+
+		console.log(response);
+		res.status(201).json(response);
+	} else {
+		res.status(500).json('Error in posting review');
+	}
+});
 
 // Server setup to listen.
 app.listen(PORT, () => 
